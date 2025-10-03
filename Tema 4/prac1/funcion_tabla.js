57504a7c35408efbad6c1edeed1f52a98360c061
Miguel Durán <migueld202035@gmail.com>
2025-10-03 11:56:31 +0200

    UT4 practica 1 | Terminada

--- Tema 4/prac1/funcion_tabla.js
+++ Tema 4/prac1/funcion_tabla.js
@@ -1,24 +1,20 @@
 function crearTabla(numfilas = 10,numcolumnas = 4,color = "black",iddiv) {
-    html = `<style>
-                table, td, tr {
+    let html = '';
 
-                }
-            </style>`
-
-    html += `<table>`;
+    html += `<table style = "border-collapse:collapse; border:3px solid ${color}; width: 100%;>`;
     
     for (let i = 0; i < numfilas; i++) {
 
         html += `<tr>`; 
 
         for (let j = 0; j < numcolumnas; j++) {
-            html += `<td>Hello world!</td>`;
+            html += `<td style = "border:1px solid ${color}">${i} ${j}</td>`;
         }
 
         html += `</tr>`; 
     }   
 
-    html += `</table>`
+    html += `</table>`;
 
-    document.getElementById(iddiv).innerHTML(html);
+    document.getElementById(iddiv).innerHTML += html;
 }
