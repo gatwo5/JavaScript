32d50929d2c741357b1887f91db5c39967436818
Miguel Durán <migueld202035@gmail.com>
2025-10-10 10:28:27 +0200

    UT4 Practica 3 | Solo funciona si metes un libro la primera vez

---
+++ Tema 4/prueba_mapas1/accion.js
@@ -0,0 +1,21 @@
+const arrayPalabras = [];
+let mapa;
+
+//hacemos una primera lectura adelantada
+var palabra = prompt("Escriba una palabra (o nada si desea acabar)");
+while (palabra != null && palabra != "") {
+  arrayPalabras.push(palabra);
+  palabra = prompt("Escriba una palabra (o nada si desea acabar)");
+}
+
+//ordenamos el array
+arrayPalabras.sort((a, b) => a.localeCompare(b, "es"));
+
+//convertimos en mapa
+mapa = mapaRepeticiones(arrayPalabras);
+
+//recorremos el mapa y mostramos las repeticiones
+for ([palabra, cont] of mapa) {
+  document.body.innerHTML += `<p>${palabra}, ${cont} repeticiones`;
+}
+
