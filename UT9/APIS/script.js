785264e44b05717e53d81a952c7d0f0408c7a4b3
Miguel Durán <migueld202035@gmail.com>
2026-01-14 10:15:32 +0100

    UT9 APIS | Al hacer click en el mapa se crea un marcador y se guarda en localstorage para representarlos al recargar la página

--- UT9/APIS/script.js
+++ UT9/APIS/script.js
@@ -1,3 +1,9 @@
+// ==========================
+// --- VARIABLES GLOBALES ---
+// ==========================
+
+var marcadores = JSON.parse(localStorage.getItem('marcadores'));
+
 // ===========
 // --- DOM ---
 // ===========
@@ -32,6 +38,42 @@ function hover_mapa(e) {
     DOM.longitud.innerHTML = `Y: ${longitud}`;
 }
 
+function agregar_marcador(e) {
+    const latitud = e.latlng.lat;
+    const longitud = e.latlng.lng;
+
+    // JSON para pasar a localstorage
+
+    const latlang = {
+        lat: latitud,
+        lon: longitud
+    }
+
+    // Se agrega el marcador 
+
+    L.marker([latitud,longitud]).addTo(map);
+
+    // Se guarda el marcador en localStorage
+
+    if (marcadores !== null) {
+        marcadores.push(latlang);
+    } else {
+        marcadores = [latlang];
+    }
+
+    localStorage.setItem('marcadores', JSON.stringify(marcadores));
+}
+
+function mostrar_todos_marcadores() {
+
+    console.log(marcadores);
+
+    if (marcadores !== null) {
+        marcadores.forEach(marcador => {
+            L.marker([marcador.lat, marcador.lon]).addTo(map);
+        })
+    }
+}
 
 
 // ===============
@@ -39,4 +81,10 @@ function hover_mapa(e) {
 // ===============
 
 map.on('mousemove', hover_mapa);
+map.on('click', agregar_marcador);
 
+// ===========================
+// --- AL CARGAR LA PÁGINA ---
+// ===========================
+
+mostrar_todos_marcadores();
